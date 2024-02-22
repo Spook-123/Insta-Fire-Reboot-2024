4748c036158dc172584cfa9416f6d23d0072061a
Sumeet Jawalikar <97434294+Spook-123@users.noreply.github.com>
2024-02-22 20:59:47 +0530

    Update FcmApi.kt

--- app/src/main/java/com/sumeet/insta_fire/push_notification/FcmApi.kt
+++ app/src/main/java/com/sumeet/insta_fire/push_notification/FcmApi.kt
@@ -8,7 +8,7 @@ import retrofit2.http.POST
 
 interface FcmApi {
 
-    @Headers("Content-Type: application/json", "Authorization: Bearer AAAAtZkXM3I:APA91bHNU2AtZ8q8-uyluydjHuQixpC4jSLqyGbWxXoEC_WprWJx6T3iTRywz9n-3WHieuqFLd3NFx9AivnhGMRTjlBFBaC6nsr4zwkhCJKGHpLmHimTiGmhWavh7erGjeG-PDLRj_zR")
+    @Headers("Content-Type: application/json", "Authorization: Bearer")
     @POST("fcm/send")
     suspend fun sendNotification(@Body notification: NotificationData): Response<Void>
 }
